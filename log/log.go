005423e99116afb362363f673ec3adc1a27b52cc
Cristian Anghel <59115261+Ginger955@users.noreply.github.com>
2024-07-15 12:28:28 +0300

    remove TODO note

--- log/log.go
+++ log/log.go
@@ -7,8 +7,6 @@ import (
 	"time"
 )
 
-//TODO: add global logger such that it is not required to define a Logger object every time?
-
 type Logger struct {
 	buf          []byte
 	level        level.Level
